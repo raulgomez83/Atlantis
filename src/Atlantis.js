0650240dcaa252fd354adfd1c7be48626b87ea8f
Raul Gomez <83raulgomez@gmail.com>
2020-11-15 17:09:10 +0100

    parallax

---
+++ src/Atlantis.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export const Atlantis = () => {
+    return (
+        <div className="wrapper">
+            <section className="section parallax bg1">
+                <h1>Enjoy</h1>
+            </section>
+            <section className="section static">
+               <h1>Travel</h1>
+            </section>
+            <section className="section parallax bg2">
+               <h1>Atlantis</h1>
+            </section>
+        </div>
+    )
+}
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import "./styles/styles.scss"
+
+import { Atlantis } from './Atlantis';
+
+
+ReactDOM.render(
+    <Atlantis />,
+  document.getElementById('root')
+);
