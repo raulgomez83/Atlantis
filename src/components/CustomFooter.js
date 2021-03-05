501316f7ab761f26c460dd56df0478658e650569
Raul Gomez <83raulgomez@gmail.com>
2021-03-05 16:33:48 +0100

    detalles (falta nombre y CIF)

--- src/components/CustomFooter.js
+++ src/components/CustomFooter.js
@@ -1,12 +1,11 @@
 import React from "react";
+import image from "../images/igape.png";
 
 export const CustomFooter = () => {
   return (
     <div className="footer__parrafo">
-      <p>
-        ©Atlantis by Anet{" "}
-        <span>(Página creada con la financiación de la Xunta de Galicia)</span>
-      </p>
+      <p>©Atlantis by Anet</p>
+      <img className="image__footer" src={image} alt="logos Xunta IGAPE" />
     </div>
   );
 };
--- src/components/PoliticaPrivacidad.js
+++ src/components/PoliticaPrivacidad.js
@@ -9,174 +9,168 @@ export const PoliticaPrivacidad = () => {
                     Email: reservas@atlantistravel.es`;
   return (
     <div className="aviso__legal">
-      <div>
-        <h3>
-          IDENTIFICACIÓN Y DATOS DE CONTACTO DEL RESPONSABLE DEL TRATAMIENTO
-        </h3>
-        <p className="aviso_p">
-          <b>{nombreLegal}</b> , sociedad domiciliada <b>{contactoCompleto}</b>,
-          con CIF {CIF}, es el responsable del tratamiento de los datos de
-          carácter personal que se traten a través de esta página web. En la
-          presente cláusula se facilita la información sobre el uso que
-          realizará la Sociedad de tus datos personales. Asimismo, la presente
-          Política de Privacidad tiene por objeto facilitar información sobre
-          los derechos que asisten tanto al Usuario registrados como no
-          registrado en virtud del Reglamento General de Protección de Datos
-          (“RGPD”). Si tienes cualquier duda relativa al tratamiento de tus
-          datos personales, contacta con la Sociedad en la siguiente
-          <b>{contactoCompleto}</b>. Asimismo, te informamos que DEAN IVAN
-          HALLOWS cuenta con un Delegado de Protección de Datos designado, con
-          quién te podrás poner en contacto a través de la siguiente
-          <b>{contactoCompleto}</b>
-        </p>
-        <h4>DONDE INFORMA</h4>
-        <p className="aviso_p">
-          En las siguiente web: http://atlantisbyanet.com/(+ información en
-          nuestro aviso legal) TRATAMIENTO DE DATOS
-        </p>
-        <p className="aviso_p">
-          <b>{nombreLegal}</b> informa a través de estas web sobre su política
-          de privacidad respecto del tratamiento y protección de los datos de
-          carácter personal de cualquier titular con el que mantenga una
-          relación en la que se requiera el tratamiento de sus datos. Las
-          operaciones, gestiones y procedimientos técnicos que se realicen de
-          forma automatizada o no automatizada y que posibiliten la recogida, el
-          almacenamiento, la modificación, la transferencia y otras acciones
-          sobre datos de carácter personal, tienen la consideración de
-          tratamiento de datos personales, y en este caso informamos de un
-          tratamiento conjunto en aquellos casos necesarios para la gestión de
-          servicios y/o productos contratados a cualquiera de las empresas
-          anteriormente indicadas, sin perjuicio de lo indicado en el apartado 4
-          de esta política.
-        </p>
-        <h4>LEGITIMACIÓN DEL TRATAMIENTO</h4>
-        <p className="aviso_p">
-          La base de la legitimación del tratamiento de Datos Personales será la
-          que resulte de la relación contractual o precontractual, la laboral o
-          cualquier otra que se requiera para el tratamiento de datos, tales
-          como el consentimiento expreso.
-        </p>
-        <h4>COMUNICACIONES ELECTRÓNICAS</h4>
-        <p className="aviso_p">
-          En el caso de recibir comunicaciones por estos medios (correos
-          electrónicos, mensajes de respuesta automatizada de formularios, y
-          otros sistemas de comunicación) le informamos que los mensajes se
-          dirigen exclusivamente a su destinatario y puede contener información
-          privilegiada o confidencial. Si no es vd. el destinatario indicado,
-          queda notificado de que la utilización, divulgación y/o copia sin
-          autorización está prohibida en virtud de la legislación vigente.
-        </p>
-        <p className="aviso_p">
-          De conformidad con lo establecido en la Ley 34/2002 de 11 de julio, de
-          Servicios de la Sociedad de la Información y del Comercio Electrónico,
-          y la Directiva 2002/58/CE le comunicamos que en el supuesto de que no
-          desee recibir comunicaciones e informaciones de índole comercial
-          mediante este sistema de comunicación electrónica, nos lo indique por
-          esta misma vía indicando en el asunto BAJA COMUNICACIONES para que sus
-          datos personales sean dados de baja de nuestra base de datos. Su
-          solicitud será accionada en un plazo de 10 días desde su envío. En el
-          supuesto de que no recibamos contestación expresa por su parte,
-          entenderemos que acepta y autoriza que nuestra empresa siga realizando
-          las referidas comunicaciones.
-        </p>
-        <h4>RESPONSABLES DE TRATAMIENTO</h4>
-        <p className="aviso_p">
-          En cumplimiento de la legislación vigente en Protección de Datos le
-          indicamos que los datos personales que Ud. nos facilita de forma
-          voluntaria por cualquiera de nuestros medios de recogida de
-          información serán incorporados a tratamientos automatizados y/o
-          manuales, siendo <b>{nombreLegal}</b> con la que mantiene la relación
-          contractual el Responsable de dichos tratamientos. Los datos no se
-          cederán a terceros salvo en los casos en que exista una obligación
-          legal.
-        </p>
-        <p className="aviso_p">
-          Asimismo, <b>{nombreLegal}</b> declara y garantiza que mantiene los
-          niveles de seguridad para la protección de datos personales conforme
-          dispone la legislación vigente, y que ha establecido todos los medios
-          técnicos a su alcance para evitar la pérdida, mal uso, alteración,
-          acceso no autorizado, robo de los datos facilitados por los
-          interesados y para que sus empleados observen dichas prescripciones,
-          así como el deber de secreto sobre los datos objeto de la protección y
-          demás obligaciones de confidencialidad vigentes.
-        </p>
-        <p className="aviso_p">
-          Los datos serán conservados mientras dure la relación que genera dicho
-          tratamiento o durante los años necesarios para el cumplir las
-          obligaciones legales. Una vez finalizada la relación que justifica el
-          tratamiento de datos, estos serán bloqueados conforme a lo dispuesto
-          en la normativa, transcurrido el plazo legalmente establecido serán
-          eliminados (ver en cada finalidad el plazo de conservación).
-        </p>
-        <p className="aviso_p">
-          La normativa de protección de datos le confiere al titular de los
-          datos personales los siguientes derechos:
-        </p>
-        <ul>
-          <li>
-            Derecho a revocar cualquier consentimiento prestado previamente.
-          </li>
-          <li>
-            Derecho de acceso: Conocer qué tipo de datos se están tratando y las
-            características del tratamientoque se lleva a cabo.
-          </li>
-          <li>
-            Derecho de rectificación: Poder solicitar la modificación de los
-            datos que sean inexactos o no veraces
-          </li>
-          <li>
-            Derecho de portabilidad: Poder obtener una copia en formato
-            interoperable de los datos que esténsiendo tratados
-          </li>
-          <li>
-            Derecho a la limitación del tratamiento en los casos que considere
-            que no es necesario
-          </li>
-          <li>
-            Derecho de supresión: Solicitar el cese del tratamiento de los datos
-            y su supresión cuando ya no seanecesaria su conservación
-          </li>
-          <li>
-            Derecho a interponer una reclamación ante la Agencia Española de
-            Protección de datos C/ Jorge Juan, 6 28001-Madrid – FAX: 914483680-
-            TELF: 901 100 099- E-mail: ciudadanos@agpd.es
-          </li>
-        </ul>
-        <p className="aviso_p">
-          Si desea más información respecto al tratamiento de sus datos,
-          rectificar aquellos que sean inexactos, oponerse y/o limitar algún
-          tratamiento que considere que no es necesario, o solicitar la
-          cancelación del tratamiento cuando los datos ya no sean necesarios,
-          puede dirigirse por escrito a:{contactoCompleto}.<br /> Dicha
-          comunicación deberá reflejar la siguiente información: Nombre y
-          apellidos del usuario, la petición de solicitud, el domicilio y los
-          datos acreditativos. o El ejercicio de derechos deberá ser realizado
-          por el propio usuario. No obstante, podrán ser ejecutados por una
-          persona autorizada como representante legal del autorizado. En tal
-          caso, se deberá aportar la documentación que acredite esta
-          representación del interesado.
-        </p>
-        <h4>FINALIDADES DE LOS TRATAMIENTOS DE DATO</h4>
-        <p className="aviso_p">
-          Detallamos a continuación las finalidades de los tratamientos de datos
-          llevados a cabo por el Responsable de Tratamiento:
-        </p>
-        <ul>
-          <li>CLIENTES: Gestión de la relación con los clientes.</li>
-          <li>PROVEEDORES: Gestión de la relación con los proveedores.</li>
-          <li>
-            CONTABLE Y FISCAL: Datos necesarios para el cumplimiento de las
-            obligaciones fiscales ycontables de la empresa.
-          </li>
-          <li>
-            LABORAL: Gestión de la relación con laboral con los empleados.
-          </li>
-          <li>
-            RRHH: Gestión de la relación con los candidatos a un empleo en la
-            empresa.
-          </li>
-        </ul>
-      </div>
+      <h3>
+        IDENTIFICACIÓN Y DATOS DE CONTACTO DEL RESPONSABLE DEL TRATAMIENTO
+      </h3>
+      <p className="aviso_p">
+        <b>{nombreLegal}</b> , sociedad domiciliada <b>{contactoCompleto}</b>,
+        con CIF {CIF}, es el responsable del tratamiento de los datos de
+        carácter personal que se traten a través de esta página web. En la
+        presente cláusula se facilita la información sobre el uso que realizará
+        la Sociedad de tus datos personales. Asimismo, la presente Política de
+        Privacidad tiene por objeto facilitar información sobre los derechos que
+        asisten tanto al Usuario registrados como no registrado en virtud del
+        Reglamento General de Protección de Datos (“RGPD”). Si tienes cualquier
+        duda relativa al tratamiento de tus datos personales, contacta con la
+        Sociedad en la siguiente
+        <b>{contactoCompleto}</b>. Asimismo, te informamos que DEAN IVAN HALLOWS
+        cuenta con un Delegado de Protección de Datos designado, con quién te
+        podrás poner en contacto a través de la siguiente
+        <b>{contactoCompleto}</b>
+      </p>
+      <h4>DONDE INFORMA</h4>
+      <p className="aviso_p">
+        En las siguiente web: http://atlantisbyanet.com/(+ información en
+        nuestro aviso legal) TRATAMIENTO DE DATOS
+      </p>
+      <p className="aviso_p">
+        <b>{nombreLegal}</b> informa a través de estas web sobre su política de
+        privacidad respecto del tratamiento y protección de los datos de
+        carácter personal de cualquier titular con el que mantenga una relación
+        en la que se requiera el tratamiento de sus datos. Las operaciones,
+        gestiones y procedimientos técnicos que se realicen de forma
+        automatizada o no automatizada y que posibiliten la recogida, el
+        almacenamiento, la modificación, la transferencia y otras acciones sobre
+        datos de carácter personal, tienen la consideración de tratamiento de
+        datos personales, y en este caso informamos de un tratamiento conjunto
+        en aquellos casos necesarios para la gestión de servicios y/o productos
+        contratados a cualquiera de las empresas anteriormente indicadas, sin
+        perjuicio de lo indicado en el apartado 4 de esta política.
+      </p>
+      <h4>LEGITIMACIÓN DEL TRATAMIENTO</h4>
+      <p className="aviso_p">
+        La base de la legitimación del tratamiento de Datos Personales será la
+        que resulte de la relación contractual o precontractual, la laboral o
+        cualquier otra que se requiera para el tratamiento de datos, tales como
+        el consentimiento expreso.
+      </p>
+      <h4>COMUNICACIONES ELECTRÓNICAS</h4>
+      <p className="aviso_p">
+        En el caso de recibir comunicaciones por estos medios (correos
+        electrónicos, mensajes de respuesta automatizada de formularios, y otros
+        sistemas de comunicación) le informamos que los mensajes se dirigen
+        exclusivamente a su destinatario y puede contener información
+        privilegiada o confidencial. Si no es vd. el destinatario indicado,
+        queda notificado de que la utilización, divulgación y/o copia sin
+        autorización está prohibida en virtud de la legislación vigente.
+      </p>
+      <p className="aviso_p">
+        De conformidad con lo establecido en la Ley 34/2002 de 11 de julio, de
+        Servicios de la Sociedad de la Información y del Comercio Electrónico, y
+        la Directiva 2002/58/CE le comunicamos que en el supuesto de que no
+        desee recibir comunicaciones e informaciones de índole comercial
+        mediante este sistema de comunicación electrónica, nos lo indique por
+        esta misma vía indicando en el asunto BAJA COMUNICACIONES para que sus
+        datos personales sean dados de baja de nuestra base de datos. Su
+        solicitud será accionada en un plazo de 10 días desde su envío. En el
+        supuesto de que no recibamos contestación expresa por su parte,
+        entenderemos que acepta y autoriza que nuestra empresa siga realizando
+        las referidas comunicaciones.
+      </p>
+      <h4>RESPONSABLES DE TRATAMIENTO</h4>
+      <p className="aviso_p">
+        En cumplimiento de la legislación vigente en Protección de Datos le
+        indicamos que los datos personales que Ud. nos facilita de forma
+        voluntaria por cualquiera de nuestros medios de recogida de información
+        serán incorporados a tratamientos automatizados y/o manuales, siendo{" "}
+        <b>{nombreLegal}</b> con la que mantiene la relación contractual el
+        Responsable de dichos tratamientos. Los datos no se cederán a terceros
+        salvo en los casos en que exista una obligación legal.
+      </p>
+      <p className="aviso_p">
+        Asimismo, <b>{nombreLegal}</b> declara y garantiza que mantiene los
+        niveles de seguridad para la protección de datos personales conforme
+        dispone la legislación vigente, y que ha establecido todos los medios
+        técnicos a su alcance para evitar la pérdida, mal uso, alteración,
+        acceso no autorizado, robo de los datos facilitados por los interesados
+        y para que sus empleados observen dichas prescripciones, así como el
+        deber de secreto sobre los datos objeto de la protección y demás
+        obligaciones de confidencialidad vigentes.
+      </p>
+      <p className="aviso_p">
+        Los datos serán conservados mientras dure la relación que genera dicho
+        tratamiento o durante los años necesarios para el cumplir las
+        obligaciones legales. Una vez finalizada la relación que justifica el
+        tratamiento de datos, estos serán bloqueados conforme a lo dispuesto en
+        la normativa, transcurrido el plazo legalmente establecido serán
+        eliminados (ver en cada finalidad el plazo de conservación).
+      </p>
+      <p className="aviso_p">
+        La normativa de protección de datos le confiere al titular de los datos
+        personales los siguientes derechos:
+      </p>
+      <ul>
+        <li>
+          Derecho a revocar cualquier consentimiento prestado previamente.
+        </li>
+        <li>
+          Derecho de acceso: Conocer qué tipo de datos se están tratando y las
+          características del tratamientoque se lleva a cabo.
+        </li>
+        <li>
+          Derecho de rectificación: Poder solicitar la modificación de los datos
+          que sean inexactos o no veraces
+        </li>
+        <li>
+          Derecho de portabilidad: Poder obtener una copia en formato
+          interoperable de los datos que esténsiendo tratados
+        </li>
+        <li>
+          Derecho a la limitación del tratamiento en los casos que considere que
+          no es necesario
+        </li>
+        <li>
+          Derecho de supresión: Solicitar el cese del tratamiento de los datos y
+          su supresión cuando ya no seanecesaria su conservación
+        </li>
+        <li>
+          Derecho a interponer una reclamación ante la Agencia Española de
+          Protección de datos C/ Jorge Juan, 6 28001-Madrid – FAX: 914483680-
+          TELF: 901 100 099- E-mail: ciudadanos@agpd.es
+        </li>
+      </ul>
+      <p className="aviso_p">
+        Si desea más información respecto al tratamiento de sus datos,
+        rectificar aquellos que sean inexactos, oponerse y/o limitar algún
+        tratamiento que considere que no es necesario, o solicitar la
+        cancelación del tratamiento cuando los datos ya no sean necesarios,
+        puede dirigirse por escrito a:{contactoCompleto}.<br /> Dicha
+        comunicación deberá reflejar la siguiente información: Nombre y
+        apellidos del usuario, la petición de solicitud, el domicilio y los
+        datos acreditativos. o El ejercicio de derechos deberá ser realizado por
+        el propio usuario. No obstante, podrán ser ejecutados por una persona
+        autorizada como representante legal del autorizado. En tal caso, se
+        deberá aportar la documentación que acredite esta representación del
+        interesado.
+      </p>
+      <h4>FINALIDADES DE LOS TRATAMIENTOS DE DATO</h4>
+      <p className="aviso_p">
+        Detallamos a continuación las finalidades de los tratamientos de datos
+        llevados a cabo por el Responsable de Tratamiento:
+      </p>
+      <ul>
+        <li>CLIENTES: Gestión de la relación con los clientes.</li>
+        <li>PROVEEDORES: Gestión de la relación con los proveedores.</li>
+        <li>
+          CONTABLE Y FISCAL: Datos necesarios para el cumplimiento de las
+          obligaciones fiscales ycontables de la empresa.
+        </li>
+        <li>LABORAL: Gestión de la relación con laboral con los empleados.</li>
+        <li>
+          RRHH: Gestión de la relación con los candidatos a un empleo en la
+          empresa.
+        </li>
+      </ul>
     </div>
   );
 };
